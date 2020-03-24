6190a3c2ee4ed2e087d0d4543530500d28cd3e53
Juan Carlos Serrano <juancarlos.serrano.chicharro@bbvaitspain.com>
2020-03-24 17:01:33 +0100

    Extract method with side effect - SRP

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,35 @@ main(process.argv[2])
 async function main (filepath: string): Promise<void> {
 
 	try {
-		const filepathResolved = path.resolve(filepath)
-		const data = await fs.promises.readFile(filepathResolved, 'utf8')
+		const data = await input(filepath)
 
 		const wordCount = data.split(/\s/).length
 		const wordCountJSON = {
 			wordCount
 		}
 
-		return fs.promises.writeFile(
-			'word-count.json',
-			JSON.stringify(wordCountJSON, null, 2)
-		)
+		await output(wordCountJSON)
 	} catch (e) {
 		console.error(e)
 		process.exit(1)
 	}
 }
+
+function input (filepath: string): Promise<string> {
+	const filepathResolved = path.resolve(filepath)
+	return fs.promises.readFile(filepathResolved, 'utf8')
+}
+
+type OutPut = (content: { wordCount: number }) => Promise<void>
+
+function saveTo (filepath: string): OutPut {
+	return content => {
+		const filepathResolved = path.resolve(filepath)
+		return fs.promises.writeFile(
+			filepathResolved,
+			JSON.stringify(content, null, 2)
+		)
+	}
+}
+
+const output: OutPut = saveTo('word-count.json')
